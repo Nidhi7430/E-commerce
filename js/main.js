6b2a3cd349009daaa23bb8b4547fc5171b3aabce
Nidhi7430 <nidhipatel439@gmail.com>
2021-01-08 14:40:56 +0530

    add banner nd showcase slider

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.banner_slider_otr').slick({
+        infinite: true,
+        dots:true,
+        arrows:false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+  });
+
+  $(document).ready(function(){
+    $('.showcase_bottom_slider').slick({
+    });
+  });
